39db8f533548b3171cf4ecc29cdf029de3061e62
jaeyo03 <toyazx@naver.com>
2024-09-02 23:19:35 +0900

    refactor : delete component

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,6 @@ function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        로고 클릭시 공식 문서로 이동. Vite + React 가 프론트 아키텍처의 시작
-      </p> */}
       <br></br>
       <Tryon></Tryon>
       <br></br>
